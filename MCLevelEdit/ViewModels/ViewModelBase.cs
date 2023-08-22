51cdbf6a9e39cdd52c98a976af70bdeb9ad3161f
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-08-22 21:59:03 +0200

    feaT: initial Avalonia template check-in.

---
+++ MCLevelEdit/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace MCLevelEdit.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ MCLevelEdit/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace MCLevelEdit.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
