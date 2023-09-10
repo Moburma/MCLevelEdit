cb491249c29179bcc1e3400111ae7f00aa696c0d
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-09-10 23:40:32 +0200

    feat: implemented basic empty datagrid for entities

--- MCLevelEdit/ViewModels/EntitiesViewModel.cs
+++ MCLevelEdit/ViewModels/EntitiesViewModel.cs
@@ -1,16 +1,24 @@
 ﻿using MCLevelEdit.DataModel;
+using ReactiveUI;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MCLevelEdit.ViewModels
 {
     public class EntitiesViewModel : ViewModelBase
     {
+        public ObservableCollection<Entity> Entities { get; }
+        public ICommand AddNewEntityCommand { get; }
+
         public EntitiesViewModel()
         {
             Entities = new ObservableCollection<Entity>();
             Entities.Add(new Entity(0, DataModel.EntityTypes.I.Spawns[0], new Position(0, 0)));
-        }
 
-        public ObservableCollection<Entity> Entities { get; }
+            AddNewEntityCommand = ReactiveCommand.Create(() =>
+            {
+                // Code here will be executed when the button is clicked.
+            });
+        }
     }
 }
---
+++ MCLevelEdit/Views/EntitiesView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace MCLevelEdit.Views
+{
+    public partial class EntitiesView : UserControl
+    {
+        public EntitiesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
