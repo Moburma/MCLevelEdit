69c46e5523da7a4aa9345e48b1c27740627c85da
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-09-12 23:14:54 +0200

    feat: added TypeIds to data model

--- MCLevelEdit/ViewModels/EntitiesViewModel.cs
+++ MCLevelEdit/ViewModels/EntitiesViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using MCLevelEdit.DataModel;
 using ReactiveUI;
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
 using System.Windows.Input;
 
 namespace MCLevelEdit.ViewModels
@@ -8,6 +12,7 @@ namespace MCLevelEdit.ViewModels
     public class EntitiesViewModel : ViewModelBase
     {
         public ObservableCollection<Entity> Entities { get; }
+        public ObservableCollection<KeyValuePair<int, string>> TypeIds { get; }
 
         public ICommand AddNewEntityCommand { get; }
 
@@ -17,6 +22,11 @@ namespace MCLevelEdit.ViewModels
             {
                 new Entity(0, DataModel.EntityTypes.I.Spawns[(int)Spawn.Flyer1], new Position(0, 0))
             };
+
+            TypeIds = (ObservableCollection<KeyValuePair<int, string>>)Enum.GetValues(typeof(TypeId))
+                .Cast<int>()
+                .Select(x => new KeyValuePair<int, string>(key: x, value: Enum.GetName(typeof(TypeId), x)))
+                .ToObservable<KeyValuePair<int, string>>();
 
             AddNewEntityCommand = ReactiveCommand.Create(() =>
             {
