c2f532bebd834e9a7c831116287400da18e7b0e2
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-10-13 00:03:24 +0200

    feat: made Entities Window

--- MCLevelEdit/ViewModels/MainViewModel.cs
+++ MCLevelEdit/ViewModels/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MCLevelEdit.Interfaces;
 using ReactiveUI;
 using Splat;
+using System.Reactive.Linq;
 using System.Windows.Input;
 
 namespace MCLevelEdit.ViewModels;
@@ -9,14 +10,17 @@ public class MainViewModel : ViewModelBase
 {
     public ICommand EditEntitiesCommand { get; }
     public EntitiesViewModel EntitiesViewModel { get; }
+    public Interaction<EntitiesViewModel, EntitiesViewModel?> ShowDialog { get; }
 
     public MainViewModel(IMapService mapService) : base(mapService)
     {
         EntitiesViewModel = Locator.Current.GetService<EntitiesViewModel>();
 
+        ShowDialog = new Interaction<EntitiesViewModel, EntitiesViewModel?>();
+
         EditEntitiesCommand = ReactiveCommand.CreateFromTask(async () =>
         {
-
+            var result = await ShowDialog.Handle(EntitiesViewModel);
         });
     }
 }
--- MCLevelEdit/Views/MainWindow.axaml.cs
+++ MCLevelEdit/Views/MainWindow.axaml.cs
@@ -1,11 +1,26 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia.ReactiveUI;
+using MCLevelEdit.ViewModels;
+using ReactiveUI;
+using System.Threading.Tasks;
 
 namespace MCLevelEdit.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        this.WhenActivated(action => action(ViewModel!.ShowDialog.RegisterHandler(DoShowDialogAsync)));
+    }
+
+    private async Task DoShowDialogAsync(InteractionContext<EntitiesViewModel,
+                                    EntitiesViewModel?> interaction)
+    {
+        var dialog = new EntitiesWindow();
+        dialog.DataContext = interaction.Input;
+
+        var result = await dialog.ShowDialog<EntitiesViewModel?>(this);
+        interaction.SetOutput(result);
     }
 }
