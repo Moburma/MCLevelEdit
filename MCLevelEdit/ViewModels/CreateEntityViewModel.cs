e80341d3361f72ed33f0fedbb281294f204b403b
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-10-16 23:31:04 +0200

    feat: added more fields to CreateEntityViewModel

--- MCLevelEdit/ViewModels/CreateEntityViewModel.cs
+++ MCLevelEdit/ViewModels/CreateEntityViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MCLevelEdit.DataModel;
+﻿using Avalonia;
+using MCLevelEdit.DataModel;
 using MCLevelEdit.Interfaces;
 using ReactiveUI;
 using System;
@@ -7,9 +8,15 @@ using System.Linq;
 
 namespace MCLevelEdit.ViewModels
 {
-    public class CreateEntityViewModel : ReactiveObject
-    { 
-        public Entity Entity { get; private set; }
+    public class CreateEntityViewModel : ViewModelBase
+    {
+        private Entity _entity;
+
+        public Entity Entity
+        {
+            get => _entity;
+            set => this.RaiseAndSetIfChanged(ref _entity, value);
+        }
 
         public static KeyValuePair<int, string>[] TypeIds { get; } =
             Enum.GetValues(typeof(TypeId))
@@ -17,9 +24,14 @@ namespace MCLevelEdit.ViewModels
             .Select(x => new KeyValuePair<int, string>(key: x, value: Enum.GetName(typeof(TypeId), x)))
             .ToArray();
 
-        public CreateEntityViewModel(IMapService mapService)
+        public CreateEntityViewModel(IMapService mapService) : base(mapService)
         {
-            Entity = new Entity();
+            Entity = new Entity()
+            {
+                Id = Map.Entities.Count(),
+                EntityType = new SpawnType(Spawn.Flyer1),
+                Position = new Position(0, 0)
+            };
         }
     }
 }
