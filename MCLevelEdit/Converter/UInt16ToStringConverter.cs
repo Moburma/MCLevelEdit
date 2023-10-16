1bc033606b6a088aafad9a9875922fd0afc9ce74
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-10-16 21:58:20 +0200

    refactor: moved number to string converter to its own method

---
+++ MCLevelEdit/Converter/UInt16ToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace MCLevelEdit.Converter
+{
+    public class UInt16ToStringConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value.ToString();
+            }
+            return "0";
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var i = 0;
+                int.TryParse(value.ToString(), out i);
+                return (i < 0 || i > ushort.MaxValue ? 0 : i);
+            }
+            return 0;
+        }
+    }
+}
--- MCLevelEdit/DataModel/TerrainGenerationParameters.cs
+++ MCLevelEdit/DataModel/TerrainGenerationParameters.cs
@@ -5,29 +5,6 @@ using System.Globalization;
 
 namespace MCLevelEdit.DataModel
 {
-    public class ParameterConverter : IValueConverter
-    {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            if (value != null)
-            {
-                return value.ToString();
-            }
-            return "0";
-        }
-
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            if (value != null)
-            {
-                var i = 0;
-                int.TryParse(value.ToString(), out i);
-                return (i < 0 || i > ushort.MaxValue ? 0 : i);
-            }
-            return 0;
-        }
-    }
-
     public class TerrainGenerationParameters : ObservableObject
     {
         private ushort _seed;
