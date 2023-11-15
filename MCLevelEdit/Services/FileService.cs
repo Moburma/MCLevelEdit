6048363864dca2d33eebb4612341f30d3344071a
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-11-15 23:26:58 +0100

    feat: implemented get of entities from map file

--- MCLevelEdit/Services/FileService.cs
+++ MCLevelEdit/Services/FileService.cs
@@ -1,6 +1,5 @@
 ﻿using MCLevelEdit.DataModel;
 using System;
-using System.Data;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -38,10 +37,125 @@ namespace MCLevelEdit.Services
                 RkSte = BitConverter.ToUInt16(levfile, 44)
             };
 
+            int fpos = 1090; // Move on to Thing data
+
+            var entityCount = 1999;
+
             Map map = new Map()
             {
-                TerrainGenerationParameters = terrainGenerationParameters
+                TerrainGenerationParameters = terrainGenerationParameters,
             };
+
+            do
+            {
+                counter++;
+                // Get Thing entries
+                TypeId entityTypeId = (TypeId)BitConverter.ToUInt16(levfile, fpos);
+                //string ThingType = IdentifyThing(classValue);
+                ushort childId = BitConverter.ToUInt16(levfile, fpos + 2);
+                ushort Xpos = BitConverter.ToUInt16(levfile, fpos + 4);
+                ushort Ypos = BitConverter.ToUInt16(levfile, fpos + 6);
+                ushort DisId = BitConverter.ToUInt16(levfile, fpos + 8);
+                ushort SwiSz = BitConverter.ToUInt16(levfile, fpos + 10);
+                ushort SwiId = BitConverter.ToUInt16(levfile, fpos + 12);
+                ushort Parent = BitConverter.ToUInt16(levfile, fpos + 14);
+                ushort Child = BitConverter.ToUInt16(levfile, fpos + 16);
+
+                string ThingName = "";
+
+                var entityType = EntityTypeExtensions.GetEntityFromTypeIdAndChildId(entityTypeId, childId);
+
+                if (entityType != null)
+                {
+                    map.Entities.Add(new Entity()
+                    {
+                        Id = counter,
+                        EntityType = entityType,
+                        Position = new Position(Xpos, Ypos),
+                        Parent = Parent,
+                        Child = Child
+                    });
+                }
+
+                //if (classValue == 0)
+                //{
+                //    ThingName = "Blank";
+                //}
+                //else if (classValue == 2)
+                //{
+                //    ThingName = IdentifyScenery(Model);
+                //    if (drawMap == 1)
+                //    {
+                //        bmp.SetPixel(Xpos, Ypos, Color.Green);
+                //    }
+                //}
+                //else if (classValue == 3)
+                //{
+                //    ThingName = IdentifySpawn(Model);
+                //    if (drawMap == 1)
+                //    {
+                //        bmp.SetPixel(Xpos, Ypos, Color.Yellow);
+                //    }
+                //}
+                //else if (classValue == 5)
+                //{
+                //    ThingName = IdentifyCreature(Model);
+                //    if (drawMap == 1)
+                //    {
+                //        bmp.SetPixel(Xpos, Ypos, Color.Red);
+                //    }
+                //}
+                //else if (classValue == 7)
+                //{
+                //    ThingName = IdentifyWeather(Model);
+                //}
+                //else if (classValue == 10)
+                //{
+                //    ThingName = IdentifyEffect(Model);
+                //    if (drawMap == 1)
+                //    {
+                //        bmp.SetPixel(Xpos, Ypos, Color.Cyan);
+                //    }
+                //}
+                //else if (classValue == 11)
+                //{
+                //    ThingName = IdentifySwitch(Model);
+                //    if (drawMap == 1)
+                //    {
+                //        bmp.SetPixel(Xpos, Ypos, Color.White);
+                //    }
+                //}
+                //else if (classValue == 12)
+                //{
+                //    ThingName = IdentifySpell(Model);
+                //    if (drawMap == 1)
+                //    {
+                //        bmp.SetPixel(Xpos, Ypos, Color.Purple);
+                //    }
+                //}
+
+                // Define Thing datatable rows
+                //DataRow row = datatable.NewRow();
+
+                //row["thingno"] = ThingNo;
+                //row["class"] = classValue;
+                //row["ThingTypeHidden"] = ThingType;
+                //row["Model"] = Model;
+                //row["ThingNameHidden"] = ThingName;
+                //row["XPos"] = Xpos;
+                //row["YPos"] = Ypos;
+                //row["DisId"] = DisId;
+                //row["swisz"] = SwiSz;
+                //row["SwiId"] = SwiId;
+                //row["parent"] = Parent;
+                //row["child"] = Child;
+                //datatable.Rows.Add(row);
+
+                //ThingCount--;
+
+                fpos += 18;
+                entityCount--;
+            } while (entityCount != 0);
 
             return Task.FromResult(map);
         }
