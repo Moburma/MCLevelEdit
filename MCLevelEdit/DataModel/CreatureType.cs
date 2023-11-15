a9d5d70ca75710d184c6a1b93e13c259fb2292f9
Timothy Hobbs <thobbsinteractive@gmail.com>
2023-11-15 23:25:11 +0100

    fix: added missing creature types

--- MCLevelEdit/DataModel/CreatureType.cs
+++ MCLevelEdit/DataModel/CreatureType.cs
@@ -16,7 +16,13 @@ namespace MCLevelEdit.DataModel
         TrollOrApe = 7,
         Griffin = 8,
         Skeleton = 9,
-        Emu = 10
+        Emu = 10,
+        Genie = 11,
+        Builder = 12,
+        Townie = 13,
+        Trader = 14,
+        Unknown = 15,
+        Wyvern = 16
     }
 
     public class CreatureType : EntityType
